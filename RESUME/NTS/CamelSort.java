b5f5cfda4b3d596d0fef2bb63daccdd36e9fbade
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-09 02:01:32 +0900

    Create CamelSort.java

---
+++ RESUME/NTS/CamelSort.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Melissa on 12/9/17.
+ */
+    public class tryThree {
+        public static void findCapital(String input){
+            StringBuffer sb = new StringBuffer(input);
+            int i = 0;
+
+            //char[] newCharArray = input.toCharArray();
+            for(i = 0; i < sb.length() ; i++){
+
+                if( sb.charAt(i) == 95){
+                    char ch = sb.charAt(i+1);
+
+                    //System.out.print(sb.charAt(i));
+                    sb.delete(i, i + 2);
+                    sb.insert(i, String.valueOf((char)(ch - 32)));
+                    i--;
+                }
+            }
+            System.out.println(sb);
+        }
+
+        public static void main(String[] args) {
+            findCapital("mine_craft_world");
+
+        }
+
+    }
