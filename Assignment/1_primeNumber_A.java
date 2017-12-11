00ffc4a52b5fff6393be3a8f129099d7bc9a5f7c
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-11 15:11:05 +0900

    Create 1_primeNumber_A.java

---
+++ Assignment/1_primeNumber_A.java
@@ -0,0 +1,22 @@
+//에라토스테네스의 체(소수 구하기)
+class Solution {
+    public static void main(String[] args) {
+        boolean[] num = new boolean[100];
+        for (int i = 2; i < num.length; i++) {
+            num[i] = true;
+        }
+        for (int i = 2; i < num.length; i++) {
+            int k = 2;
+            for (int index = i * k; index < num.length; index = i * k++) {
+                if (num[index]) {
+                    num[index] = false;//소수가 아닌것을 표시
+                }
+            }
+        }
+        for (int i = 2; i < num.length; i++) {
+            if (num[i] == true) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
