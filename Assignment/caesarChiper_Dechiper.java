fe3c2fa883f20fcfb7167a867c8baeef5a1154c0
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-11 21:37:18 +0900

    Update caesarChiper_Dechiper.java

--- Assignment/caesarChiper_Dechiper.java
+++ Assignment/caesarChiper_Dechiper.java
@@ -3,7 +3,11 @@ public class Assignment_2 {
         StringBuffer sb = new StringBuffer();
         for(int i = 0; i < input.length() ; i++){
             char x = input.charAt(i);
-             if( 'z' - x < num){
+            //check for space
+            if(x == 32){
+                sb.append((char)32);
+            }
+             else if( 'z' - x < num){
                  sb.append((char)('a' + (num-('z'- x)-1)));
              }else{
                  sb.append((char)(x + num));
@@ -16,7 +20,11 @@ public class Assignment_2 {
         StringBuffer sb = new StringBuffer();
         for(int i = 0; i < input.length() ; i++){
             char x = input.charAt(i);
-            if( x - num < 'a'){
+            //check for space
+            if(x == 32) {
+                sb.append((char)32);
+            }
+            else if( x - num < 'a'){
                 sb.append((char)('z' - (num-(x-'a')-1)));
             }else{
                 sb.append((char)(x - num));
@@ -28,7 +36,7 @@ public class Assignment_2 {
 
 
     public static void main(String[] args) {
-        caesarCipher("abcxyz", 3);
-        caesarDecipher("defabc", 3);
+        caesarCipher("melissa gu", 3);
+        caesarDecipher("pholvvd jx", 3);
     }
 }
