7a312d223bacb6963bc38ef233c3aa6aea1ea7db
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-11 14:36:10 +0900

    Update and rename 1 to 1_primeNumber.java

---
+++ Assignment/1_primeNumber.java
@@ -0,0 +1,25 @@
+
+public class Assignment_1 {
+    public static void primenumber(int input) {
+        boolean decider;
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 2; i <= input; i++) {
+            int right = 1;
+            for (int j = 2; j < i; j++) {
+
+                if (i % j == 0 && i != 2) {
+                    right = 0;
+                }
+            }
+            if (right == 1) {
+                sb.append(i + " ");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) {
+        primenumber(100);
+    }
+}
