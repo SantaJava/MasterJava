820cb203aab6f16f1dfa938b1852654319a8deac
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-11 21:19:31 +0900

    Update and rename caesarChiper.java to caesarChiper_Dechiper.java

--- Assignment/caesarChiper_Dechiper.java
+++ Assignment/caesarChiper_Dechiper.java
@@ -12,7 +12,23 @@ public class Assignment_2 {
         System.out.println(sb);
     }
 
+    public static void caesarDecipher(String input, int num){
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < input.length() ; i++){
+            char x = input.charAt(i);
+            if( x - num < 'a'){
+                sb.append((char)('z' - (num-(x-'a')-1)));
+            }else{
+                sb.append((char)(x - num));
+            }
+        }
+        System.out.println(sb);
+    }
+
+
+
     public static void main(String[] args) {
-        caesarCipher("vwxyz", 3);
+        caesarCipher("abcxyz", 3);
+        caesarDecipher("defabc", 3);
     }
 }
