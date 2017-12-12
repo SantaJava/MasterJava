251bdadce7cb13227c9b0ffab20a03ae8615d1c3
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-12 22:10:48 +0900

    Create 4_Palindrome_words.java

---
+++ Assignment/4_Palindrome_words.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+/**
+ * Created by Melissa on 12/11/17.
+ */
+public class Palindrome {
+
+    public static boolean solution(String input) {
+        Boolean isP = true;
+        Stack<Character> stack = new Stack<Character>();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < input.length(); i++) {
+            char a = input.charAt(i);
+            stack.push(a);
+        }
+
+        while (stack.isEmpty() == false) {
+            sb.append(stack.pop());
+        }
+        System.out.print(String.valueOf(sb));
+
+
+        if (input.equals(String.valueOf(sb))){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(solution("POOP"));
+    }
+}
