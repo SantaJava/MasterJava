6babde9ee0471dcff854a1895baf6c90e06cd7cc
SantaJava <33744796+SantaJava@users.noreply.github.com>
2017-12-16 16:09:37 +0900

    Create Day14_scope.java

---
+++ HackerRank30Days/Day14_scope.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    
+    public Difference(int[] a) {
+
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+
+        }
+        elements = a;
+
+    }
+
+    public void computeDifference() {
+        maximumDifference = elements[elements.length-1] - elements[0];
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
