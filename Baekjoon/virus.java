5005fdace1f33ee5be3a84f3c057bc88ad6816e6
img31415 <karidy11@gmail.com>
2020-02-25 21:09:28 +0900

    DFS

---
+++ Baekjoon/virus.java
@@ -0,0 +1,96 @@
+package Baekjoon;
+
+/*
+ *2606 find the computers that are going to be infected with virus.
+ * */
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+class DfsGraph {
+    private int nV; //Node개수
+    private ArrayList<ArrayList<Integer>> dfsGraph;
+    private boolean[] visitArr;
+
+    public DfsGraph(int nV) {
+        this.nV = nV;
+        this.dfsGraph = new ArrayList<ArrayList<Integer>>();
+        // 그래프 초기화
+        for (int i = 0; i < this.nV + 1; i++) {
+            this.dfsGraph.add(new ArrayList<Integer>());
+        }
+        this.visitArr = new boolean[this.nV + 1];
+    }
+
+    public ArrayList<ArrayList<Integer>> getGraph() {
+        return this.dfsGraph;
+    }
+
+    public ArrayList<Integer> getNode(int i) {
+        return this.dfsGraph.get(i);
+    }
+
+    //양방향 그래프
+    public void put(int x, int y) {
+        this.dfsGraph.get(x).add(y);
+        this.dfsGraph.get(y).add(x);
+    }
+
+    //단방향 그래프
+    public void putSingle(int x, int y) {
+        this.dfsGraph.get(x).add(y);
+    }
+
+    //그래프 출력(인접 리스트)
+    public void printGraphToAdjList() {
+        for (int i = 1; i < this.dfsGraph.size(); i++) {
+            System.out.println("정점" + i + "의 인접 리스트");
+            for (int j = 0; j < this.dfsGraph.get(i).size(); j++) {
+                System.out.println("->" + this.dfsGraph.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+
+    public void clearVisitArr() {
+        for (int i = 0; i < this.visitArr.length; i++) {
+            this.visitArr[i] = false;
+        }
+    }
+
+    public void dfs(int vIdx) {
+        this.visitArr[vIdx] = true;
+        System.out.println(vIdx + " ");
+        for (int i : this.dfsGraph.get(vIdx)) {
+            if (this.visitArr[i] == false) {
+                dfs(i);
+            }
+        }
+    }
+}
+
+public class virus {
+
+    public static void main(String[] args) throws Exception {
+        int nV = 0;
+        int nE = 0;
+
+        Scanner sc = new Scanner(System.in);
+        nV = Integer.parseInt(sc.nextLine()); //정점
+        nE = Integer.parseInt(sc.nextLine()); //간선
+        DfsGraph dfsGraph = new DfsGraph(nV);
+
+        for (int i = 0; i < nE; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            dfsGraph.put(a, b);
+        }
+
+        sc.close();
+        dfsGraph.dfs(1);
+    }
+}
--- Kakao/bm.java
+++ Kakao/bm.java
@@ -9,8 +9,8 @@ import java.util.regex.*;
 public class bm {
     public static void main(String args[]) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine().trim());
         double xMat[][]; //X 행렬
         double yMat[][]; //Y 행렬
         double rMat[][]; //R 행렬
@@ -19,7 +19,7 @@ public class bm {
         int k = 0;
 
         for (int i = 0; i < T; i++) {
-            String[] inputArr = br.readLine().split(" ");
+            String[] inputArr = sc.nextLine().split(" ");
             n = Integer.parseInt(inputArr[0]);
             m = Integer.parseInt(inputArr[1]);
             k = Integer.parseInt(inputArr[2]);
@@ -27,21 +27,23 @@ public class bm {
             yMat = new double[k][k];
             rMat = new double[n][m];
 
-            //이중 행렬 x,y값을 저장한다.
+            //행렬 X 값 저장합니다.
             for (int x = 0; x < k; x++) {
-                String[] tempArr = br.readLine().split(" ");
+                String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < k; j++) {
                     xMat[x][j] = Double.parseDouble(tempArr[j]);
                 }
             }
+            //행렬 Y값을 저장합니다.
             for (int x = 0; x < k; x++) {
-                String[] tempArr = br.readLine().split(" ");
+                String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < k; j++) {
                     yMat[x][j] = Double.parseDouble(tempArr[j]);
                 }
             }
+            //행렬 R의 값을 저장합니다.
             for (int x = 0; x < n; x++) {
-                String[] tempArr = br.readLine().trim().split(" ");
+                String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < m; j++) {
                     rMat[x][j] = Double.parseDouble(tempArr[j]);
                 }
@@ -55,10 +57,10 @@ public class bm {
                             testMat[o][j] = rMat[l + o][r + j];
                         }
                     }
-                    if(solution(xMat, yMat, testMat, k)) count++;
+                    if (solution(xMat, yMat, testMat, k)) count++; //행렬 R에서 만들 수 있는 모든 행렬들을 검사합니다.
                 }
             }
-            System.out.println(count+1);
+            System.out.println(count);
         }
     }
 
@@ -71,44 +73,31 @@ public class bm {
         }
     }
 
-    public static boolean solution(double[][] xMat, double[][] yMat, double[][] testMat, int k) {
+    public static boolean solution(double[][] xMat, double[][] yMat, double[][] zMat, int k) {
+        //xz = y
+        double[] v = new double[k];
+        while (--k > 0) {
+            randomize(v);
+            double[] expected = product(v, yMat);
+            double[] result = product(product(v, zMat), xMat);
 
-        double[][] r = new double[k][1];
-        Random random = new Random();
-        for (int i = 0; i < k; i++) {
-            r[i][0] = random.nextInt(2);
+            if (!Arrays.equals(expected, result)) return false;
         }
-
-        double tr[][] = new double[k][1];
-        double yr[][] = new double[k][1];
-        double xtr[][] = new double[k][1];
-
-        tr = multiply(testMat, r, k);
-        yr = multiply(yMat, r, k);
-        xtr = multiply(xMat, tr, k);
-
-        boolean result = true;
-
-        for (int i = 0; i < k; i++) {
-            if (xtr[i][0] == 0) {
-                continue;
-            } else {
-                result = false;
-            }
-        }
-
-        return result;
+        return true;
     }
 
-    public static double[][] multiply(double[][] x, double[][] y, int k) {
-        double result[][] = new double[k][1];
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < 1; j++) {
-                for (int z = 0; z < k; z++) {
-                    result[i][j] = result[i][j] + x[i][z] * y[z][j];
-                }
-            }
+    public static double[] product(double[] v, double[][] mat) {
+        int N = mat.length;
+        int[] vector = new int[N];
+        for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) vector[i] += v[j] * mat[i][j];
+        return v;
+    }
+
+
+    public static void randomize(double[] v) {
+        //random v 행열을 만들어줍니다.
+        for (int i = 0; i < v.length; i++) {
+            v[i] = (Math.random() < 0.5) ? 0 : 1; //0과 1 중 하나의 수를 저장합니다.
         }
-        return result;
     }
 }
