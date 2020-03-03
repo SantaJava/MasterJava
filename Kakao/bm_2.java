8077ee981ab5e03ef007838bc270ec8db2f08a96
img31415 <karidy11@gmail.com>
2020-03-03 18:48:42 +0900

    freivalds

--- Kakao/bm_2.java
+++ Kakao/bm_2.java
@@ -6,14 +6,14 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class bm {
+public class bm_2 {
     public static void main(String args[]) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
         Scanner sc = new Scanner(System.in);
         int T = Integer.parseInt(sc.nextLine().trim());
-        double xMat[][]; //X 행렬
-        double yMat[][]; //Y 행렬
-        double rMat[][]; //R 행렬
+        int xMat[][]; //X 행렬
+        int yMat[][]; //Y 행렬
+        int rMat[][]; //R 행렬
         int n = 0;
         int m = 0;
         int k = 0;
@@ -23,32 +23,32 @@ public class bm {
             n = Integer.parseInt(inputArr[0]);
             m = Integer.parseInt(inputArr[1]);
             k = Integer.parseInt(inputArr[2]);
-            xMat = new double[k][k];
-            yMat = new double[k][k];
-            rMat = new double[n][m];
+            xMat = new int[k][k];
+            yMat = new int[k][k];
+            rMat = new int[n][m];
 
             //행렬 X 값 저장합니다.
             for (int x = 0; x < k; x++) {
                 String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < k; j++) {
-                    xMat[x][j] = Double.parseDouble(tempArr[j]);
+                    xMat[x][j] = Integer.parseInt(tempArr[j]);
                 }
             }
             //행렬 Y값을 저장합니다.
             for (int x = 0; x < k; x++) {
                 String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < k; j++) {
-                    yMat[x][j] = Double.parseDouble(tempArr[j]);
+                    yMat[x][j] = Integer.parseInt(tempArr[j]);
                 }
             }
             //행렬 R의 값을 저장합니다.
             for (int x = 0; x < n; x++) {
                 String[] tempArr = sc.nextLine().split(" ");
                 for (int j = 0; j < m; j++) {
-                    rMat[x][j] = Double.parseDouble(tempArr[j]);
+                    rMat[x][j] = Integer.parseInt(tempArr[j]);
                 }
             }
-            double[][] testMat = new double[k][k];
+            int[][] testMat = new int[k][k];
             int count = 0;
             for (int l = 0; l < rMat.length - k + 1; l++) {
                 for (int r = 0; r < rMat[l].length - k + 1; r++) {
@@ -57,46 +57,78 @@ public class bm {
                             testMat[o][j] = rMat[l + o][r + j];
                         }
                     }
-                    printArr(testMat);
-                    if (solution(xMat, yMat, testMat, k)) count++; //행렬 R에서 만들 수 있는 모든 행렬들을 검사합니다.
+                    if (solution(xMat, yMat, testMat, k)) count++;
+                    //행렬 R에서 만들 수 있는 모든 행렬들을 검사합니다.
                 }
             }
-            System.out.println(count);
+          //  System.out.println(count);
         }
     }
 
-    public static void printArr(double[][] arr) {
+    public static void printArr(int[][] arr) {
         //print array to check answers
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.println(arr[i][j] + " ");
+                System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
         }
     }
 
-    public static boolean solution(double[][] xMat, double[][] yMat, double[][] zMat, int k) {
+    public static boolean solution(int[][] xMat, int[][] yMat, int[][] zMat, int k) {
         //xz = y
-        double[] v = new double[k];
+        int[] v = new int[k];
+        printArr(zMat);
         while (--k > 0) {
             randomize(v);
-            /*double[] expected = product(v, yMat);
-            double[] result = product(product(v, zMat), xMat);
-
-            if (!Arrays.equals(expected, result)) return false;*/
-
-
+            product(xMat, zMat, yMat, v);
+            //System.out.print(result + " ");
         }
         return true;
     }
 
-    public static double[] product(double[] v, double[][] mat) {
-        int N = mat.length;
-        int[] vector = new int[N];
-        for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) vector[i] += v[j] * mat[i][j];
-        return v;
+    public static boolean product(int[][] A, int[][] B, int[][] C, int[] v) {
+        int k = A.length;
+        int[] Br = new int[k];
+        int[] Cr = new int[k];
+        int[] ABr = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            int elementB = 0;
+            int elementC = 0;
+
+            for (int j = 0; j < k; j++) {
+                elementB += B[i][j] * v[i];
+            }
+            Br[i] = elementB;
+
+            for (int j = 0; j < k; j++) {
+                elementC += C[i][j] * v[i];
+            }
+            Cr[i] = elementC;
+        }
+
+        for (int i = 0; i < k; i++) {
+            int elementAB = 0;
+            for (int j = 0; j < k; k++) {
+                elementAB += A[i][j] * v[i];
+            }
+            ABr[i] = elementAB;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (ABr[i] - Cr[i] != 0) {
+                System.out.println("false!");
+                return false;
+            }
+        }
+        System.out.println("?");
+
+
+        return true;
     }
 
-    public static void randomize(double[] v) {
+    public static void randomize(int[] v) {
         //random v 행열을 만들어줍니다.
         for (int i = 0; i < v.length; i++) {
             v[i] = (Math.random() < 0.5) ? 0 : 1; //0과 1 중 하나의 수를 저장합니다.
